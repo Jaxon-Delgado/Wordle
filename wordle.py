95a71c4a9d67bdcb0079de8fb53ec3ec822352b1
Jaxon-Delgado <jax@vt.edu>
2022-04-19 21:05:53 -0400

    fixed word being the same every time you play

--- wordle.py
+++ wordle.py
@@ -14,7 +14,9 @@ PLAYER_INSTRUCTIONS = "You may start guessing\n"
 GUESS_STATEMENT = "\nEnter your guess"
 REPLAY = "\nWould you like to Play? (Y/n)"
 ALLOWED_GUESSES = 6
+WORDLIST = word_list
 
+# Create colored letters while playing
 def correct_place(letter):
     return f'[black on green]{letter}[/]'
 
@@ -28,8 +30,11 @@ def incorrect_letter(letter):
 
 
 def check_guess(guess, answer):
+    # Initiate arrays
     guessed = []
     wordle_pattern = []
+
+    # Put user guesses into their respective arrays
     for i, letter in enumerate(guess):
         if answer[i] == guess[i]:
             guessed += correct_place(letter)
@@ -49,22 +54,32 @@ def game(console, chosen_word):
     full_wordle_pattern = []
     all_words_guessed = []
 
+    # Start game loop
     while not end_of_game:
         guess = Prompt.ask(GUESS_STATEMENT).upper()
+
+        # Check for length as well as already guessed words
         while len(guess) != 5 or guess in already_guessed:
             if guess in already_guessed:
                 console.print("[red]You've already guessed this word!!\n[/]")
             else:
                 console.print('[red]Please enter a 5-letter word!!\n[/]')
             guess = Prompt.ask(GUESS_STATEMENT).upper()
+
+        # Append guesses and check guesses to each array
         already_guessed.append(guess)
         guessed, pattern = check_guess(guess, chosen_word)
         all_words_guessed.append(guessed)
         full_wordle_pattern.append(pattern)
 
+        # Print the current turn
         console.print(*all_words_guessed, sep="\n")
+
+        # End game if user guesses correct word
         if guess == chosen_word or len(already_guessed) == ALLOWED_GUESSES:
             end_of_game = True
+    
+    # Check against turns and guesses and end the game
     if len(already_guessed) == ALLOWED_GUESSES and guess != chosen_word:
         console.print(f"\n[red]WORDLE X/{ALLOWED_GUESSES}[/]")
         console.print(f'\n[green]Correct Word: {chosen_word}[/]')
@@ -74,14 +89,17 @@ def game(console, chosen_word):
 
 
 if __name__ == '__main__':
+    # Initiate game in console
     console = Console()
-    chosen_word = choice(word_list)
     console.print(WELCOME_MESSAGE)
     console.print(PLAYER_INSTRUCTIONS)
     replay = True
+
+    # Loop through the game to continue playing as long as you want
     while replay:
         replay_event = Prompt.ask(REPLAY).upper()
         if replay_event == 'Y':
+            chosen_word = choice(word_list)
             game(console, chosen_word)
         elif replay_event == 'N':
             replay = False
